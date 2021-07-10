6d19dfc5995a7b5303113dd17f37666a2d2f75bd
nasibullin1412 <57040048+nasibullin1412@users.noreply.github.com>
2021-07-11 00:15:24 +0300

    add scroll view and bottom navigation bar with indicators

--- app/src/main/java/com/homework/nasibullin/MainActivity.kt
+++ app/src/main/java/com/homework/nasibullin/MainActivity.kt
@@ -2,10 +2,13 @@ package com.homework.nasibullin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        val cardView: CardView = findViewById(R.id.cv_movie_card)
+        cardView.setBackgroundResource(R.drawable.sh_card_view_back)
     }
 }
