61f7b98b2b59d2bd8f60b764608191db9e784dbd
nasibullin1412 <57040048+nasibullin1412@users.noreply.github.com>
2021-07-17 17:34:13 +0300

    fix offsets of item

--- app/src/main/java/com/homework/nasibullin/MainActivity.kt
+++ app/src/main/java/com/homework/nasibullin/MainActivity.kt
@@ -2,9 +2,11 @@ package com.homework.nasibullin
 
 import android.content.Context
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.util.DisplayMetrics
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,8 +30,8 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
     private lateinit var movieAdapter: MovieAdapter
     private lateinit var genreModel: GenreModel
     private lateinit var movieModel: MovieModel
-
-
+    private var movieItemWidth: Int = 0
+    private var movieItemMargin: Int = 0
 
 
     /*
@@ -44,18 +46,25 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
         setContentView(R.layout.list_of_movies)
         initDataSource()
         setupViews()
+        showToast(screenWidth.toString())
     }
 
     /*
     * Init data models
     * */
-    private fun initDataSource(){
+    private fun initDataSource() {
         movieModel = MovieModel(MoviesDataSourceImpl())
         genreModel = GenreModel(MovieGenreSourceImpl())
     }
 
 
+    private val screenWidth: Int
+        get() {
+            return windowManager.defaultDisplay.width
+        }
+
     private fun setupViews() {
+        calculateValues()
         prepareMovieGenreRecycleView()
         prepareMovieRecycleView()
     }
@@ -63,9 +72,9 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
     /*
     * Get device orientation
      */
-    private val Context.orientation:Boolean
+    private val Context.orientation: Boolean
         get() {
-            return when(resources.configuration.orientation){
+            return when (resources.configuration.orientation) {
                 Configuration.ORIENTATION_PORTRAIT -> true
                 Configuration.ORIENTATION_LANDSCAPE -> false
                 Configuration.ORIENTATION_UNDEFINED -> true
@@ -76,12 +85,12 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
     /*
     * Movie genre recycle view with ListAdapter
     * */
-    private fun prepareMovieGenreRecycleView(){
+    private fun prepareMovieGenreRecycleView() {
         movieGenreRecycler = findViewById(R.id.rvMovieGenreList)
         genreAdapter = GenreAdapter(this)
         genreAdapter.initOnClickInterface(this)
         genreAdapter.submitList(genreModel.getGenres())
-        val itemDecorator = GenreItemDecoration(leftRight= GENRE_LEFT_RIGHT_OFFSET)
+        val itemDecorator = GenreItemDecoration(leftRight = GENRE_LEFT_RIGHT_OFFSET)
         movieGenreRecycler.addItemDecoration(itemDecorator)
         movieGenreRecycler.adapter = genreAdapter
         movieGenreRecycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
@@ -90,19 +99,51 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
     /*
     * Movie recycle view with ListAdapter
     * */
-    private fun prepareMovieRecycleView(){
+    private fun prepareMovieRecycleView() {
         movieRecycler = findViewById(R.id.rvMovieList)
         movieAdapter = MovieAdapter(this)
         movieAdapter.initOnClickInterface(this)
         movieAdapter.submitList(movieModel.getMovies())
-        val itemDecorator = MovieItemDecoration(topBottom= MOVIE_TOP_BOTTOM_OFFSET, right= MOVIE_RIGHT_OFFSET)
+        val itemDecorator = MovieItemDecoration(
+            topBottom = MOVIE_TOP_BOTTOM_OFFSET,
+            right = calculateOffset(),
+            spanNumber = getSpanNumber() - 1
+        )
         movieRecycler.addItemDecoration(itemDecorator)
         movieRecycler.adapter = movieAdapter
-        movieRecycler.layoutManager = GridLayoutManager(this,
-            if (orientation)  PORTRAIT_ORIENTATION_SPAN_NUMBER else LANDSCAPE_ORIENTATION_SPAN_NUMBER,
+        movieRecycler.layoutManager = GridLayoutManager(
+            this,
+            getSpanNumber(),
             RecyclerView.VERTICAL,
-            false)
+            false
+        )
 
+    }
+
+    /*
+    * Get number of span depending on orientation
+    * */
+    private fun getSpanNumber(): Int =
+        if (orientation) PORTRAIT_ORIENTATION_SPAN_NUMBER else LANDSCAPE_ORIENTATION_SPAN_NUMBER
+
+    /*
+    * Calculate offset item movies depending on orientation
+    * */
+    private fun calculateOffset(): Int {
+        var offset: Int = if (orientation) {
+            screenWidth - movieItemWidth * 2 - movieItemMargin * 2
+        } else {
+            (screenWidth - movieItemWidth * 3 - movieItemMargin * 2) / 2
+        }
+        val density = resources.displayMetrics.density
+        offset = (offset.toFloat()/density).toInt()
+        return offset
+    }
+
+
+    private fun calculateValues() {
+        movieItemMargin = resources.getDimension(R.dimen.list_of_guideline_left).toInt()
+        movieItemWidth = resources.getDimension(R.dimen.img_movie_poster_width).toInt()
     }
 
     /*
@@ -110,48 +151,51 @@ class MainActivity : AppCompatActivity(), OnClickListenerInterface {
     * */
     private fun showToast(message: String?) {
         when {
-            message.isNullOrEmpty() -> { showToast(ERROR_MESSAGE) }
+            message.isNullOrEmpty() -> {
+                showToast(ERROR_MESSAGE)
+            }
             else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
     }
+}
 
 /*
 * Lesson 3 code
 * */
-    /*private fun init(){
-        setCorrectShapeToCardView()
-        prepareRecycleView()
-    }
+        /*private fun init(){
+            setCorrectShapeToCardView()
+            prepareRecycleView()
+        }
 
-    /*
-     * This function gives the correct shape (with top-right and top-left radius) to card view
-     * */
-    private fun setCorrectShapeToCardView()
-    {
-        cardView = findViewById(R.id.cvMovieCard)
-        cardView.setBackgroundResource(R.drawable.sh_card_view_back)
-    }
+        /*
+         * This function gives the correct shape (with top-right and top-left radius) to card view
+         * */
+        private fun setCorrectShapeToCardView()
+        {
+            cardView = findViewById(R.id.cvMovieCard)
+            cardView.setBackgroundResource(R.drawable.sh_card_view_back)
+        }
 
-    /*
-    * Card view initialization and launch function
-    * */
-    private fun prepareRecycleView(){
-        recycler = findViewById(R.id.rvActorsList)
-        val actors: List<Actor> = prepareActors()
-        adapter = ActorAdapter(this, actors)
-        recycler.adapter = adapter
-        recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-    }
+        /*
+        * Card view initialization and launch function
+        * */
+        private fun prepareRecycleView(){
+            recycler = findViewById(R.id.rvActorsList)
+            val actors: List<Actor> = prepareActors()
+            adapter = ActorAdapter(this, actors)
+            recycler.adapter = adapter
+            recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        }
 
-    /*
-    * Data initialization function to create a recycle view
-    * */
-    private fun prepareActors(): List<Actor>{
-        return listOf(
-            Actor(R.drawable.jason_statham, R.string.str_first_actor_name),
-            Actor(R.drawable.holt__mc_callany, R.string.str_second_actor_name),
-            Actor(R.drawable.josh_hartnett, R.string.str_third_actor_name)
-        )
-    }*/
+        /*
+        * Data initialization function to create a recycle view
+        * */
+        private fun prepareActors(): List<Actor>{
+            return listOf(
+                Actor(R.drawable.jason_statham, R.string.str_first_actor_name),
+                Actor(R.drawable.holt__mc_callany, R.string.str_second_actor_name),
+                Actor(R.drawable.josh_hartnett, R.string.str_third_actor_name)
+            )
+        }*/
 
-}
+
