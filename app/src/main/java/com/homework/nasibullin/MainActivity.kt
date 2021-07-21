46e87532d8ad6f6fe6b92f619195c5fdf1884b71
nasibullin1412 <57040048+nasibullin1412@users.noreply.github.com>
2021-07-22 00:49:20 +0300

    add actions to navigation bottom and interaction of fragments MainFragment and ProfileFragment

--- app/src/main/java/com/homework/nasibullin/MainActivity.kt
+++ app/src/main/java/com/homework/nasibullin/MainActivity.kt
@@ -1,81 +1,113 @@
 package com.homework.nasibullin
 
+
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.homework.nasibullin.fragments.MainFragment
 import com.homework.nasibullin.fragments.MovieDetailsFragment
+import com.homework.nasibullin.fragments.ProfileFragment
 
 
-class MainActivity : AppCompatActivity(){
-
-    /*private lateinit var cardView: CardView
-    private lateinit var recycler: RecyclerView
-    private lateinit var adapter: ActorAdapter*/
-
+class MainActivity : AppCompatActivity() {
 
     private lateinit var mainFragment: MainFragment
     private lateinit var movieDetailsFragment: MovieDetailsFragment
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private var actionBottomFlag: Boolean = true
+
+
+    companion object {
+        const val MAIN_FRAGMENT_TAG = "mainFragment"
+        const val MOVIE_DETAIL_FRAGMENT_TAG = "movieDetailFragment"
+        const val PROFILE_FRAGMENT_TAG = "profileFragment"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainFragment = MainFragment()
-        movieDetailsFragment = MovieDetailsFragment()
+        addFragment(MAIN_FRAGMENT_TAG)
+        initNavigationListener()
+    }
+
+
+    override fun onBackPressed() {
+
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.clFragmentContainer)
+        if (fragment != null && supportFragmentManager.fragments.size != 1) {
+            fragmentTransaction.remove(fragment)
+            fragmentTransaction.commit()
+            actionBottomFlag = false
+            bottomNavigationView.selectedItemId = if (bottomNavigationView.selectedItemId == R.id.nav_profile) {
+                R.id.nav_home
+            } else {
+                R.id.nav_profile
+            }
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun initNavigationListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar)
+        bottomNavigationView.setOnItemSelectedListener {
+            actionBottom(it)
+        }
+    }
+
+    private fun actionBottom(item: MenuItem): Boolean {
+        if (actionBottomFlag){
+            when(item.itemId){
+                R.id.nav_home-> addFragment(MAIN_FRAGMENT_TAG)
+                R.id.nav_profile-> addFragment(PROFILE_FRAGMENT_TAG)
+                else -> return false
+            }
+        }
+        else{
+            actionBottomFlag = true
+        }
+        return true
+    }
+
+    private fun checkFragmentRepeat(tag: String) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val fragment: Fragment? = supportFragmentManager.findFragmentByTag(tag)
+        if (fragment != null) {
+            fragmentTransaction.remove(fragment)
+            fragmentTransaction.commit()
+        }
+    }
+
+    private fun addFragment(tag: String) {
+
+        checkFragmentRepeat(tag)
+
+
+        val fragment: Fragment = when (tag) {
+            MAIN_FRAGMENT_TAG -> {
+                MainFragment()
+            }
+            MOVIE_DETAIL_FRAGMENT_TAG -> {
+                MovieDetailsFragment()
+            }
+            PROFILE_FRAGMENT_TAG -> {
+                ProfileFragment()
+            }
+            else -> {
+                MainFragment()
+            }
+        }
+
         supportFragmentManager.beginTransaction()
-                .add(R.id.clListMovie, mainFragment)
+                .add(R.id.clFragmentContainer, fragment, tag)
                 .commit()
     }
 
-    override fun onBackPressed() {
-        for (fragment in supportFragmentManager.fragments) {
-            if (fragment.isVisible) {
-                if (fragment.childFragmentManager.backStackEntryCount > 0) {
-                    fragment.childFragmentManager.popBackStack()
-                    return
-                }
-            }
-        }
-        super.onBackPressed()
-    }
 }
-
-/*
-* Lesson 3 code
-* */
-        /*private fun init(){
-            setCorrectShapeToCardView()
-            prepareRecycleView()
-        }
-
-        /*
-         * This function gives the correct shape (with top-right and top-left radius) to card view
-         * */
-        private fun setCorrectShapeToCardView()
-        {
-            cardView = findViewById(R.id.cvMovieCard)
-            cardView.setBackgroundResource(R.drawable.sh_card_view_back)
-        }
-
-        /*
-        * Card view initialization and launch function
-        * */
-        private fun prepareRecycleView(){
-            recycler = findViewById(R.id.rvActorsList)
-            val actors: List<Actor> = prepareActors()
-            adapter = ActorAdapter(this, actors)
-            recycler.adapter = adapter
-            recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-        }
-
-        /*
-        * Data initialization function to create a recycle view
-        * */
-        private fun prepareActors(): List<Actor>{
-            return listOf(
-                Actor(R.drawable.jason_statham, R.string.str_first_actor_name),
-                Actor(R.drawable.holt__mc_callany, R.string.str_second_actor_name),
-                Actor(R.drawable.josh_hartnett, R.string.str_third_actor_name)
-            )
-        }*/
 
 
---
+++ app/src/main/java/com/homework/nasibullin/interfaces/MainFragmentClickListener.kt
@@ -0,0 +1,2 @@
+package com.homework.nasibullin.interfaces
+
