e3edbd9b87600bc4367f12b476424b53c816e584
nasibullin1412 <57040048+nasibullin1412@users.noreply.github.com>
2021-07-22 19:15:22 +0300

    work-version

--- app/src/main/java/com/homework/nasibullin/MainActivity.kt
+++ app/src/main/java/com/homework/nasibullin/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.homework.nasibullin.dataclasses.MovieDto
 import com.homework.nasibullin.fragments.MainFragment
 import com.homework.nasibullin.fragments.MovieDetailsFragment
 import com.homework.nasibullin.fragments.ProfileFragment
@@ -16,28 +15,38 @@ import com.homework.nasibullin.interfaces.MainFragmentClickListener
 
 class MainActivity : AppCompatActivity(), MainFragmentClickListener {
 
-    private lateinit var mainFragment: MainFragment
-    private lateinit var movieDetailsFragment: MovieDetailsFragment
     private lateinit var bottomNavigationView: BottomNavigationView
     private var actionBottomFlag: Boolean = true
+    private var currentFragment:String? = null
+    private var currentMovieTitle:String? = null
 
 
     companion object {
         const val MAIN_FRAGMENT_TAG = "mainFragment"
         const val MOVIE_DETAIL_FRAGMENT_TAG = "movieDetailFragment"
         const val PROFILE_FRAGMENT_TAG = "profileFragment"
+        const val CURRENT_FRAGMENT_KEY = "currentFragment"
+        const val CURRENT_MOVIE_KEY = "currentMovie"
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addFragment(MAIN_FRAGMENT_TAG)
+        if(savedInstanceState == null){
+            addFragment(MAIN_FRAGMENT_TAG)
+        }
+        else{
+            currentFragment = supportFragmentManager.fragments.last().tag
+            addFragment(currentFragment ?: MAIN_FRAGMENT_TAG,
+                    title = savedInstanceState.getString(CURRENT_MOVIE_KEY))
+        }
+
         initNavigationListener()
     }
 
 
-    private fun changeBottomItemWithoutAction(){
+    private fun changeBottomItemWithoutAction() {
         actionBottomFlag = false
         bottomNavigationView.selectedItemId = if (bottomNavigationView.selectedItemId == R.id.nav_profile) {
             R.id.nav_home
@@ -55,12 +64,15 @@ class MainActivity : AppCompatActivity(), MainFragmentClickListener {
             fragmentTransaction.remove(fragment)
             fragmentTransaction.commit()
             if (fragment.tag != MOVIE_DETAIL_FRAGMENT_TAG) {
-               changeBottomItemWithoutAction()
+                changeBottomItemWithoutAction()
             }
         } else {
             super.onBackPressed()
         }
     }
+
+
+
 
     private fun initNavigationListener() {
         bottomNavigationView = findViewById(R.id.bottom_navigation_bar)
@@ -70,9 +82,9 @@ class MainActivity : AppCompatActivity(), MainFragmentClickListener {
     }
 
     private fun actionBottom(item: MenuItem): Boolean {
-        if (actionBottomFlag){
-            when(item.itemId){
-                R.id.nav_home-> {
+        if (actionBottomFlag) {
+            when (item.itemId) {
+                R.id.nav_home -> {
                     val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
                     val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.clFragmentContainer)
                     if (fragment?.tag == MOVIE_DETAIL_FRAGMENT_TAG) {
@@ -81,11 +93,10 @@ class MainActivity : AppCompatActivity(), MainFragmentClickListener {
                     }
                     addFragment(MAIN_FRAGMENT_TAG)
                 }
-                R.id.nav_profile-> addFragment(PROFILE_FRAGMENT_TAG)
+                R.id.nav_profile -> addFragment(PROFILE_FRAGMENT_TAG)
                 else -> return false
             }
-        }
-        else{
+        } else {
             actionBottomFlag = true
         }
         return true
@@ -100,17 +111,18 @@ class MainActivity : AppCompatActivity(), MainFragmentClickListener {
         }
     }
 
-    private fun addFragment(tag: String, title: String?=null) {
-
+    private fun addFragment(tag: String, title: String? = null) {
         checkFragmentRepeat(tag)
 
+        currentMovieTitle = title
 
         val fragment: Fragment = when (tag) {
             MAIN_FRAGMENT_TAG -> {
                 MainFragment()
             }
             MOVIE_DETAIL_FRAGMENT_TAG -> {
-                MovieDetailsFragment.newInstance(title ?: throw IllegalArgumentException("Title required"))
+                MovieDetailsFragment.newInstance(currentMovieTitle
+                        ?: throw IllegalArgumentException("Title required"))
             }
             PROFILE_FRAGMENT_TAG -> {
                 ProfileFragment()
@@ -120,15 +132,26 @@ class MainActivity : AppCompatActivity(), MainFragmentClickListener {
             }
         }
 
-        supportFragmentManager.beginTransaction()
+       supportFragmentManager.beginTransaction()
                 .add(R.id.clFragmentContainer, fragment, tag)
                 .commit()
     }
 
-    override fun onMovieItemClicked(title:String) {
-        addFragment(MOVIE_DETAIL_FRAGMENT_TAG, title=title)
+
+    /**
+     * keep user genre selection when flipping screen
+     * */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(CURRENT_FRAGMENT_KEY, currentFragment)
+        outState.putString(CURRENT_MOVIE_KEY, currentMovieTitle)
     }
 
+
+    override fun onMovieItemClicked(title: String) {
+        addFragment(MOVIE_DETAIL_FRAGMENT_TAG, title = title)
+    }
 }
+
 
 
--- app/src/main/java/com/homework/nasibullin/datasourceimpl/MovieDataSourceImpl.kt
+++ app/src/main/java/com/homework/nasibullin/datasourceimpl/MovieDataSourceImpl.kt
@@ -9,7 +9,9 @@ class MoviesDataSourceImpl : MoviesDataSource {
     override fun getMovies() = listOf(
             MovieDto(
                     title = "Гнев человеческий",
-                    description = "Эйч — загадочный и холодный на вид джентльмен, но внутри него пылает жажда справедливости. Преследуя...",
+                    description = "Эйч — загадочный и холодный на вид джентльмен, но внутри него пылает жажда справедливости. " +
+                            "Преследуя свои мотивы, он внедряется в инкассаторскую компанию, чтобы выйти на соучастников серии многомиллионных ограблений," +
+                            " потрясших Лос-Анджелес. В этой запутанной игре у каждого своя роль, но под подозрением оказываются все. Виновных же обязательно постигнет гнев человеческий.",
                     rateScore = 3,
                     ageRestriction = 18,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5JP9X5tCZ6qz7DYMabLmrQirlWh.jpg",
@@ -33,7 +35,7 @@ class MoviesDataSourceImpl : MoviesDataSource {
             ),
             MovieDto(
                     title = "Мортал Комбат",
-                    description = "Боец смешанных единоборств Коул Янг не раз соглашался проиграть за деньги. Он не знает о своем наследии...",
+                    description = "Боец смешанных единоборств Коул Янг не раз соглашался проиграть за деньги. Он не знает о своем наследии и почему император Внешнего мира Шан Цзун посылает могущественного криомансера Саб-Зиро на охоту за Коулом. Янг боится за безопасность своей семьи, и майор спецназа Джакс, обладатель такой же отметки в виде дракона, как и у Коула, советует ему отправиться на поиски Сони Блейд.",
                     rateScore = 5,
                     ageRestriction = 18,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pMIixvHwsD5RZxbvgsDSNkpKy0R.jpg",
@@ -41,23 +43,24 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "боевики",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
+                                    name = "Льюис Тан",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lkW8gh20BuwzHecXqYH1eRVuWpb.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Холт МакКэллани",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aAfaMEEqD8syHv5bLi5B3sccrM2.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Джош Хартнетт",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/Am9vM77uZd9bGODugwmWtOfzx6E.jpg"
                             )
                     )
 
             ),
             MovieDto(
                     title = "Упс... Приплыли!",
-                    description = "От Великого потопа зверей спас ковчег. Но спустя полгода скитаний они готовы сбежать с него куда угодно...",
+                    description = "От Великого потопа зверей спас ковчег. Но спустя полгода скитаний они готовы сбежать с него куда угодно. Нервы на пределе. Хищники готовы забыть про запреты и заглядываются на травоядных. " +
+                            "Единственное спасение — найти райский остров. Там простор и полно еды. Но даже если он совсем близко, будут ли рады местные такому количеству гостей?",
                     rateScore = 5,
                     ageRestriction = 6,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/546RNYy9Wi5wgboQ7EtD6i0DY5D.jpg",
@@ -65,22 +68,22 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "мультфильмы",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
+                                    name = "Тара Флинн",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/17gBs4aux2NcnMvf3DK5UKUFttn.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Ава Коннолли",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/o8uE77C4wQHYHJW6En192kjxJGd.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Мэри Мюррей",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1ZRP9IfehCSx5OeBQQDcVPvKYD0.jpg"
                             )
                     )
             ),
             MovieDto(
                     title = "The Box",
-                    description = "Уличный музыкант знакомится с музыкальным продюсером, и они вдвоём отправляются в путешествие...",
+                    description = "Уличный музыкант знакомится с музыкальным продюсером, и они вдвоём отправляются в путешествие, которое перевернёт их жизни.",
                     rateScore = 4,
                     ageRestriction = 12,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fq3DSw74fAodrbLiSv0BW1Ya4Ae.jpg",
@@ -88,22 +91,22 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "драмы",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
+                                    name = "Чханёль",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qqvTuk4CTvS1IE47CUozhcHVahz.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Чо Даль Хван",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jpEPPXmVC3EDMqrDQDYyXEMYlah.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Ким Юн Сун",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fDO7vJVRkZOOY1GtQMJzf4N136q.jpg"
                             )
                     )
             ),
             MovieDto(
                     title = "Сага о Дэнни Эрнандесе",
-                    description = "Tekashi69 или Сикснайн — знаменитый бруклинский рэпер с радужными волосами — прогремел...",
+                    description = "Tekashi69 или Сикснайн — знаменитый бруклинский рэпер с радужными волосами — прогремел синглом «Gummo», коллабом с Ники Минаж, а также многочисленными преступлениями.",
                     rateScore = 2,
                     ageRestriction = 18,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5xXGQLVtTAExHY92DHD9ewGmKxf.jpg",
@@ -111,22 +114,14 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "драмы",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
-                            ),
-                            ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
-                            ),
-                            ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "6ix9ine",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xAlvyeC9zLbygGMxmmyTHymwuZP.jpg"
                             )
                     )
             ),
             MovieDto(
                     title = "Пчелка Майя",
-                    description = "Когда упрямая пчелка Майя и ее лучший друг Вилли спасают принцессу-муравьишку, начинается сказочное...",
+                    description = "Летний сбор пыльцы позади, и пчёлы пребывают в возбуждённом ожидании, ведь из столицы прибыл гонец, чтобы пригласить улей на соревнования за Кубок Мёда.",
                     rateScore = 4,
                     ageRestriction = 0,
                     imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xltjMeLlxywym14NEizl0metO10.jpg",
@@ -134,16 +129,16 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "мультфильмы",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
+                                    name = "Бенсон Джек Энтони",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aVfEldX1ksEMrx45yNBAf9MAIDZ.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Франсис Берри",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qCp0psD5qzguABpRxWmMuC04kcl.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Кристиан Харисиу",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8OpoYvO1QqBYRAp1LxxUIiRdQG0.jpg"
                             )
                     )
             ),
@@ -157,16 +152,16 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "фантастика",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = ""
+                                    name = "Эмма Стоун",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2hwXbPW2ffnXUe1Um0WXHG0cTwb.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Эмма Томпсон",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xr8Ki3CIqweWWqS5q0kUYdiK6oQ.jpg"
                             ),
                             ActorDto(
-                                    name = "",
-                                    avatarUrl = ""
+                                    name = "Джоэл Фрай",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4nEKEWJpaTHncCTv6zeP98V0qGI.jpg"
                             )
                     )
             ),
@@ -180,16 +175,16 @@ class MoviesDataSourceImpl : MoviesDataSource {
                     genre = "фантастика",
                     actors = listOf(
                             ActorDto(
-                                    name = "Джейсон Стэтхем",
-                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lldeQ91GwIVff43JBrpdbAAeYWj.jpg"
+                                    name = "Скарлетт Йоханссон",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6NsMbJXRlDZuDzatN2akFdGuTvx.jpg"
                             ),
                             ActorDto(
-                                    name = "Холт МакКэллани",
-                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8NvOcP35qv5UHWEdpqAvQrKnQQz.jpg"
+                                    name = "Флоренс Пью",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/75PvULemW8BvheSKtPMoBBsvPLh.jpg"
                             ),
                             ActorDto(
-                                    name = "Джош Хартнетт",
-                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dCfu2EN7FjISACcjilaJu7evwEc.jpg"
+                                    name = "Рэйчел Вайс",
+                                    avatarUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/x87wtRVVvsOG7hkfJlzNkkfXQCN.jpg"
                             )
                     )
             )
